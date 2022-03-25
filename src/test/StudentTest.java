346d0cf8f5e2c39c69798aa2b334d07072992241
Emilio Benavides <emilio.benavides68@gmail.com>
2022-03-25 15:07:35 -0500

    Completed JUNIT exercise

--- src/test/StudentTest.java
+++ src/test/StudentTest.java
@@ -10,53 +10,45 @@ import java.util.ArrayList;
 
 
 public class StudentTest {
-    public static final long STUDENT_ID = 1;
-    public static final String STUDENT_NAME = "tom";
 
-    @Test
-    public void getId() {
-       Student Frances = new Student(210834, "Frances");
-       long expected = 210834;
-       long actual = Frances.getId();
+    Student Maddie = new Student(786992, "Maddie");
+    double aStudent = 0;
 
-       assertEquals(expected, actual);
-    }
-
-    @Test
-    public void getName() {
-        Student Kobe = new Student(897634, "Kobe");
-        String expected = "Kobe";
-        String actual = Kobe.getName();
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void addGrade() {
-        Student Howard = new Student(123451, "Howard");
-        Howard.addGrade(94);
-        assertEquals(94, Howard.getGrades().get(0).intValue());
-    }
-
-    @Test
-    public void getGrades(){
-    Student Jerry = new Student(564732, "Jerry");
-    Jerry.addGrade(88);
-    ArrayList<Integer> gradeList = new ArrayList<>();
-    gradeList.add(88);
-    assertEquals(gradeList, Jerry.getGrades());
-    }
-
-    @Test
-    public void getGradeAverage(){
-        Student Maddie = new Student(786992, "Maddie");
+    @Before
+            public void setUp() {
         Maddie.addGrade(99);
         Maddie.addGrade(96);
         Maddie.addGrade(96);
-        double aStudent = Maddie.getGradeAverage();
-        assertEquals(97, aStudent, 0);
+        aStudent = Maddie.getGradeAverage();
+    }
+    @Test
+    public void TestGetId() {
+        assertEquals(786992, Maddie.getId());
     }
 
+    @Test
+    public void TestGetName() {
+        assertEquals("Maddie", Maddie.getName());
+    }
+
+    @Test
+    public void TestAddGrade() {
+        assertEquals(99, Maddie.getGrades().get(0).intValue());
+    }
+
+    @Test
+    public void TestGetGrades() {
+        ArrayList<Integer> gradeList = new ArrayList<>();
+        gradeList.add(99);
+        gradeList.add(96);
+        gradeList.add(96);
+        assertEquals(gradeList, Maddie.getGrades());
+    }
+
+    @Test
+    public void TestGetGradeAverage() {
+        assertEquals(97, aStudent, 0);
+    }
 }
 
 
